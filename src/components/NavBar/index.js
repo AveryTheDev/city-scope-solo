551c665d68833c3fbaf94a92512b5ce53475b52f
abrown945 <abrown945@gmail.com>
2019-09-19 10:36:45 -0400

    styling

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -9,11 +9,15 @@ import SearchBar from '../Search';
 const NavBar = () => {
     return ( 
         <div className="nav-bar">
-            <Link to="/landing" className="nav-img">
-                <img src={cityIcon} alt='Back to Home'/>                
-            </Link>
-            <h3>City Scope</h3>
-            <SearchBar />
+            <div className="nav-icon-title">
+                <Link to="/landing" className="nav-img">
+                    <img src={cityIcon} alt='Back to Home'/>                
+                </Link>
+                <h3>City Scope</h3>                
+            </div>
+            <div className="nav-search-bar">
+                <SearchBar />
+            </div>
         </div>
      );
 }
