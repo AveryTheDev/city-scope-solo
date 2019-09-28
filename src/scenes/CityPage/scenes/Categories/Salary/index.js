8a56d619ef35ede8655d0be1c6ec92a9c9a8791b
abrown945 <abrown945@gmail.com>
2019-09-28 13:44:00 -0400

    salary per profession check

--- src/scenes/CityPage/scenes/Categories/Salary/index.js
+++ src/scenes/CityPage/scenes/Categories/Salary/index.js
@@ -1,28 +1,63 @@
 import "./styles.css";
 
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import { ChosenCityContext } from "../../../../../services/context/ChosenCityContext";
-import { fetchProfessions } from "../../../../../services/api/components/categories/Salary";
+import { fetchProfessions, onProfessionSelect } from "../../../../../services/api/components/categories/Salary";
 
 const Salary = () => {
 
-    const [ professions, setProfessions ] = useState([])
+    const [ professions, setProfessions ] = useState([]);
+    const [selectedJob, setSelectedJob] = useState('');
+    const [ salaryDetails, setSalaryDetails] = useState({
+        low: '',
+        avg: '',
+        high: ''
+    })
+
+    const [menuOpen, setMenuOpen] = useState(false);
     const { chosenCity } = useContext(ChosenCityContext);
+    const salaryButton = useRef();
 
     useEffect (() => {
         (async function () {
-            setProfessions(await fetchProfessions(chosenCity.urbanScores))
+            setProfessions(await fetchProfessions(chosenCity.urbanScores));
+            setSalaryDetails(await onProfessionSelect(chosenCity.urbanScores));
         })();
-    }, [chosenCity.urbanScores])
+    }, [professions, chosenCity.urbanScores])
 
+    useEffect(() => {
+    if (menuOpen) {
+        document.addEventListener("mousedown", handleClick);
+    } else {
+        document.removeEventListener("mousedown", handleClick);
+    }
 
-    if(professions) {
-        const professionOptions = professions.map(x => <p key={x}>{x}</p>);
+    return () => {
+        document.removeEventListener("mousedown", handleClick);
+    };
+    }, [menuOpen]);
+
+    const handleClick = e => {
+        if (salaryButton.current.contains(e.target)) {
+        return;
+        } 
+        else {
+        setMenuOpen(false);
+        }
+    };
+
+    if(salaryDetails) {
+        // const professionOptions = professions.map(x => <p key={x}>{x}</p>);
+        
+        const job = selectedJob ? selectedJob : "Accountant";
         
         return (
             <div>
                 <h1>Salary</h1>
-                {professionOptions}
+                <p>{job}</p>
+                <p>25th Percentile: ${salaryDetails.low} USD</p>
+                <p>50th Percentile: ${salaryDetails.avg} USD</p>
+                <p>75th Percentile: ${salaryDetails.high} USD</p>
             </div>
         )
     }
--- src/services/api/components/categories/Salary/index.js
+++ src/services/api/components/categories/Salary/index.js
@@ -22,3 +22,32 @@ export const fetchProfessions = async urbanScores => {
 
     return professions;
 }
+
+
+export const onProfessionSelect = async (chosenCity, profession = "Accountant") => {
+    const resultWithCommas = x => {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    const city = chosenCity;
+    const jobTitle = profession;
+
+    const salaryData = (await teleport.get(
+        (await teleport.get(city)).data["_links"]["ua:salaries"]["href"]
+    )).data["salaries"];
+
+    const jobTitleData = salaryData.find(x => x["job"]["title"] === jobTitle );
+
+    const jobTitleSalaryAverages = {
+      low: resultWithCommas(
+        jobTitleData["salary_percentiles"]["percentile_25"].toFixed(2)
+      ),
+      avg: resultWithCommas(
+        jobTitleData["salary_percentiles"]["percentile_50"].toFixed(2)
+      ),
+      high: resultWithCommas(
+        jobTitleData["salary_percentiles"]["percentile_75"].toFixed(2)
+      )
+    };
+
+    return jobTitleSalaryAverages;
+};
