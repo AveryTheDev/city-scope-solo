4ad02ef54b14658081fb291335ae5261fbae2278
abrown945 <abrown945@gmail.com>
2019-10-14 08:05:27 -0400

    comparison bug

--- src/scenes/CityPage/scenes/Categories/CostOfLiving/index.js
+++ src/scenes/CityPage/scenes/Categories/CostOfLiving/index.js
@@ -4,14 +4,27 @@ import React from 'react';
 import Housing from './Housing';
 import Expenses from './Expenses';
 
-const CostOfLiving = () => {
-    return ( 
+const CostOfLiving = ({ secondCity }) => {
+
+    if(secondCity) {
+        return ( 
+            <div className="cost-of-living">
+                <h1>Cost of Living</h1>
+                <Housing secondCity/>
+                <Expenses secondCity/>
+            </div>
+        );        
+    }
+    else {
+        return (
         <div className="cost-of-living">
             <h1>Cost of Living</h1>
             <Housing />
             <Expenses />
         </div>
-     );
+        );      
+    }
+
 }
  
 export default CostOfLiving;
