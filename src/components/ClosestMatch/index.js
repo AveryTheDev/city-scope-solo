f343fbaea9187b3ba1b9c6088e3a5364a5f3122c
abrown945 <abrown945@gmail.com>
2019-10-18 05:39:04 -0400

    bug fix

--- src/components/ClosestMatch/index.js
+++ src/components/ClosestMatch/index.js
@@ -1,18 +1,40 @@
 import "./styles.css";
 
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { ChosenCityContext } from "../../services/context/ChosenCityContext";
 import { ComparisonContext } from "../../services/context/ComparisonContext";
 
-const ClosestMatch = ({secondCity}) => {
-  const [display, setDisplay] = useState(true);
+const ClosestMatch = () => {
+  const [display, setDisplay] = useState(false);
+  const [cityAlert, setCityAlert] = useState({
+    cityName: '',
+    closestCity: ''
+  })
+
   const { chosenCity } = useContext(ChosenCityContext);
-  const { comparison } = useContext(ComparisonContext)
+  const { comparison } = useContext(ComparisonContext);
+
+useEffect(() => {
+  if(!comparison.match && comparison.cityName !== comparison.closestCity) {
+    setDisplay(true);
+    setCityAlert({
+      cityName: comparison.cityName,
+      closestCity: comparison.closestCity
+    })
+  }
+  else if(!chosenCity.match && chosenCity.cityName !== chosenCity.closestCity) {
+    setDisplay(true);
+    setCityAlert({
+      cityName: chosenCity.cityName,
+      closestCity: chosenCity.closestCity
+    })
+  }
+}, [comparison, chosenCity])
 
   const closestCityStyle = display ? "closest-match" : "hidden";
 
-  if (secondCity && comparison.cityName && !comparison.match) {
-    return (
+   if(display){ 
+     return (
       <div className={closestCityStyle}>
         <div className="closest-match__container">
           <div className="closest-match__exit-container">
@@ -25,17 +47,17 @@ const ClosestMatch = ({secondCity}) => {
           </div>
           <h3 className="closest-match__header">
             <span className="closest-match--city">
-              {comparison.closestCity}
+              {cityAlert.closestCity}
             </span>{" "}
             was not found in our database, but we found a close match...
           </h3>
           <p className="closest-match__message">
             Although we don't have any information on{" "}
             <span className="closest-match--city">
-              {comparison.closestCity}
+              {cityAlert.closestCity}
             </span>
             , we figured showing you the info we had on{" "}
-            <span className="closest-match--city">{comparison.cityName}</span>{" "}
+            <span className="closest-match--city">{cityAlert.cityName}</span>{" "}
             would be the next best thing due to their close proximity to each
             other.
           </p>
@@ -44,39 +66,6 @@ const ClosestMatch = ({secondCity}) => {
     );
   }
 
-  else if (chosenCity.cityName && !chosenCity.match) {
-    return (
-      <div className={closestCityStyle}>
-        <div className="closest-match__container">
-          <div className="closest-match__exit-container">
-            <p
-              className="closest-match__exit"
-              onClick={() => setDisplay(false)}
-            >
-              X
-            </p>
-          </div>
-          <h3 className="closest-match__header">
-            <span className="closest-match--city">
-              {chosenCity.closestCity}
-            </span>{" "}
-            was not found in our database, but we found a close match...
-          </h3>
-          <p className="closest-match__message">
-            Although we don't have any information on{" "}
-            <span className="closest-match--city">
-              {chosenCity.closestCity}
-            </span>
-            , we figured showing you the info we had on{" "}
-            <span className="closest-match--city">{chosenCity.cityName}</span>{" "}
-            would be the next best thing due to their close proximity to each
-            other.
-          </p>
-        </div>
-      </div>
-    );
-  } 
-  
   else return <></>;
 };
 
