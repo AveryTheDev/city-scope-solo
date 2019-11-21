a534a200a530a3af2185e2088809c88a07260356
abrown945 <abrown945@gmail.com>
2019-11-21 14:18:26 -0500

    edge-case debugging

--- src/services/api/components/CityFromSearch/index.js
+++ src/services/api/components/CityFromSearch/index.js
@@ -1,243 +1,232 @@
 import teleport from "../../teleport";
 
 export const getCityBySearchTerm = async city => {
-    
-    let searchTerm = city;
+  let searchTerm = city;
 
-    let urbanScores, cityImage, cityName, geoname_id, id_data, coord, closestCity, results;
-    let geoname_results = {
-        geoname: 0,
-        urban_area: '',
-        match: false
+  let urbanScores,
+    cityImage,
+    cityName,
+    geoname_id,
+    id_data,
+    coord,
+    closestCity,
+    results;
+  let geoname_results = {
+    geoname: 0,
+    urban_area: "",
+    match: false
+  };
+
+  const fromCityId = async geoname_id => {
+    id_data = await teleport.get("cities/geonameid:" + geoname_id);
+    return id_data;
+  };
+
+  const getCityName = async id_data => {
+    cityName = id_data.data["name"];
+    return cityName;
+  };
+
+  const cityMatchCheck = async geoname_id => {
+    const id = geoname_id;
+
+    const cityResults = await fromCityId(id);
+
+    if (id === "1650527") {
+      return {
+        geoname: id,
+        urban_area: "",
+        match: true
+      };
     }
 
-        const fromCityId = async geoname_id => {
-            id_data = await teleport.get(
-                              "cities/geonameid:" + geoname_id
-                            ); 
-            return id_data;
-        }
+    const urban_area = cityResults.data["_links"]["city:urban_area"]["name"];
+    const city_returned = cityResults.data["name"];
 
-        const getCityName = async id_data => {
+    let check_results = {
+      geoname: 0,
+      urban_area: "",
+      match: false
+    };
 
-            cityName = id_data.data["name"]
-            return cityName;
-        }
+    if (
+      urban_area.toLowerCase() === city_returned.toLowerCase() ||
+      urban_area === "San Francisco Bay Area" ||
+      urban_area === "Tampa Bay Area" ||
+      urban_area === "Minneapolis-Saint Paul"
+    ) {
+      return (check_results = {
+        geoname: id,
+        urban_area,
+        match: true
+      });
+    } else {
+      const urban_id = await getCityIdFromUrbanName(urban_area);
 
-        const cityMatchCheck = async geoname_id => {
-            const id = geoname_id;
+      if (urban_id === id) {
+        return (check_results = {
+          geoname: id,
+          urban_area,
+          match: true
+        });
+      } else {
+        return (check_results = {
+          geoname: id,
+          urban_area,
+          match: false
+        });
+      }
+    }
+  };
 
-            const cityResults = await fromCityId(id);
+  const getCityIdFromSearchTerm = async searchTerm => {
+    if (typeof searchTerm !== "string") {
+      searchTerm = Object.values(searchTerm).toString();
+    }
 
-            if(id === '1650527') {
-                return {
-                    geoname: id,
-                    urban_area: '',
-                    match: true
-                }
-            }
+    let refinedSearchTerm = searchTerm.toLowerCase().replace(/ /g, "%20");
+    if (
+      refinedSearchTerm.includes("minneapolis-saint") ||
+      refinedSearchTerm.includes("minneapolis")
+    ) {
+      return await cityMatchCheck((geoname_id = 5045360));
+    }
 
-            const urban_area = cityResults.data["_links"]["city:urban_area"]["name"];
-            const city_returned = cityResults.data["name"];
+    if (refinedSearchTerm.includes("francisco")) {
+      return await cityMatchCheck((geoname_id = 5391959));
+    }
 
-             let check_results = {
-               geoname: 0,
-               urban_area: "",
-               match: false
-             };
+    if (new RegExp("tampa").test(refinedSearchTerm)) {
+      refinedSearchTerm = "tampa";
+    }
 
-            if(urban_area.toLowerCase() === city_returned.toLowerCase()) {
-                return check_results = {
-                    geoname: id,
-                    urban_area,
-                    match: true
-                }
-            }
-            else {
+    let citySearch = await teleport.get("cities/?search=" + refinedSearchTerm);
 
-                const urban_id = await getCityIdFromUrbanName(urban_area);
+    if (citySearch.data.count === 0)
+      return {
+        geoname: 0,
+        urban_area: "",
+        match: false
+      };
+    else {
+      let cityResponseURL =
+        citySearch.data["_embedded"]["city:search-results"][0]["_links"][
+          "city:item"
+        ]["href"];
 
-                if(urban_id === id) {
-                    return (check_results = {
-                        geoname: id,
-                        urban_area,
-                        match: true
-                    });
-                }
-                else {
-                    return check_results = {
-                        geoname: id,
-                        urban_area,
-                        match: false
-                    }                    
-                }
-            }
-        }
+      let idSearch = /[0-9]/g;
 
-        const getCityIdFromSearchTerm = async searchTerm => {
+      geoname_id = cityResponseURL.match(idSearch);
+      geoname_id = geoname_id.toString().replace(/,/g, "");
 
+      return await cityMatchCheck(geoname_id);
+    }
+  };
 
-            if(typeof searchTerm !== "string") {
-                searchTerm = (Object.values(searchTerm)).toString();
-            }
+  const getCityIdFromUrbanName = async urban_name => {
+    if (
+      urban_name.includes("minneapolis-saint") ||
+      urban_name.includes("minneapolis")
+    ) {
+      return (geoname_id = 5045360);
+    }
 
-            let refinedSearchTerm = searchTerm.toLowerCase().replace(/ /g, "%20");
-            if (refinedSearchTerm.includes("minneapolis-saint") || refinedSearchTerm.includes("minneapolis")) {
-              return geoname_id = 5045360;
-            } 
+    if (new RegExp("tampa").test(urban_name)) {
+      urban_name = "tampa";
+    }
 
-            if (new RegExp("tampa").test(refinedSearchTerm)) {
-            refinedSearchTerm = "tampa";
-            }
+    let citySearch = await teleport.get("cities/?search=" + urban_name);
 
-            let citySearch = await teleport.get(
-            "cities/?search=" + refinedSearchTerm
-            );
+    let cityResponseURL =
+      citySearch.data["_embedded"]["city:search-results"][0]["_links"][
+        "city:item"
+      ]["href"];
 
-            if(citySearch.data.count === 0) return {
-                                              geoname: 0,
-                                              urban_area: "",
-                                              match: false
-                                            };
-            else {
-                let cityResponseURL =
-                citySearch.data["_embedded"]["city:search-results"][0]["_links"][
-                    "city:item"
-                ]["href"];
+    let idSearch = /[0-9]/g;
 
-                let idSearch = /[0-9]/g;
+    geoname_id = cityResponseURL.match(idSearch);
+    geoname_id = geoname_id.toString().replace(/,/g, "");
 
-                geoname_id = cityResponseURL.match(idSearch);
-                geoname_id = geoname_id.toString().replace(/,/g, "");
+    return geoname_id;
+  };
 
-                return await cityMatchCheck(geoname_id);           
-            }
-        };
+  const getCityImageFromUrbanScores = async urbanScores => {
+    let imageURL = await teleport.get(urbanScores);
+    imageURL = imageURL.data["_links"]["ua:images"]["href"];
 
-        const getCityIdFromUrbanName = async urban_name => {
+    let cityImage = await teleport.get(imageURL);
+    cityImage = cityImage.data.photos[0].image.web;
 
-            if (
-              urban_name.includes("minneapolis-saint") ||
-              urban_name.includes("minneapolis")
-            ) {
-              return (geoname_id = 5045360);
-            }
+    return cityImage;
+  };
 
-            if (new RegExp("tampa").test(urban_name)) {
-              urban_name = "tampa";
-            }
+  const getCityDetailsFromId = async (geoname_id, id_data) => {
+    if (geoname_id === "1650527") {
+      let urbanScores = "https://api.teleport.org/api/urban_areas/slug:bali/";
+      return urbanScores;
+    } else {
+      let urbanScores = id_data.data["_links"]["city:urban_area"]["href"];
+      return urbanScores;
+    }
+  };
 
-            let citySearch = await teleport.get(
-                "cities/?search=" + urban_name
-            );
+  const getCoordFromIdData = async id_data => {
+    coord = id_data.data["location"]["latlon"];
+    return coord;
+  };
 
-            let cityResponseURL =
-                citySearch.data["_embedded"][
-                "city:search-results"
-                ][0]["_links"]["city:item"]["href"];
+  geoname_results = await getCityIdFromSearchTerm(searchTerm);
 
-            let idSearch = /[0-9]/g;
+  if (geoname_results.geoname === 0) {
+    results = {
+      urbanScores: "N/A",
+      geoname_id: "N/A",
+      cityImage: "N/A",
+      cityName: "N/A",
+      coord: "N/A",
+      match: true,
+      chosenCity: "N/A",
+      inDatabase: false
+    };
+    return results;
+  } else if (geoname_results.match) {
+    id_data = await fromCityId(geoname_results.geoname);
+    urbanScores = await getCityDetailsFromId(geoname_results.geoname, id_data);
+    cityImage = await getCityImageFromUrbanScores(urbanScores);
+    cityName = await getCityName(id_data);
+    coord = await getCoordFromIdData(id_data);
 
-            geoname_id = cityResponseURL.match(idSearch);
-            geoname_id = geoname_id
-                .toString()
-                .replace(/,/g, "");
+    results = {
+      urbanScores,
+      geoname_id: geoname_results.geoname,
+      cityImage,
+      cityName,
+      coord,
+      match: true,
+      closestCity: geoname_results.urban_area,
+      inDatabase: true
+    };
+    return results;
+  } else {
+    closestCity = await getCityName(await fromCityId(geoname_results.geoname));
+    geoname_id = await getCityIdFromUrbanName(geoname_results.urban_area);
+    id_data = await fromCityId(geoname_id);
+    urbanScores = await getCityDetailsFromId(geoname_id, id_data);
+    cityImage = await getCityImageFromUrbanScores(urbanScores);
+    cityName = await getCityName(id_data);
+    coord = await getCoordFromIdData(id_data);
+  }
 
-            return geoname_id;
-        }
+  results = {
+    urbanScores,
+    geoname_id: geoname_id,
+    cityImage,
+    cityName,
+    coord,
+    match: false,
+    closestCity,
+    inDatabase: true
+  };
 
-        const getCityImageFromUrbanScores = async urbanScores => {
-            let imageURL = await teleport.get(urbanScores);
-            imageURL = imageURL.data["_links"]["ua:images"]["href"];
-
-            let cityImage = await teleport.get(imageURL);
-            cityImage = cityImage.data.photos[0].image.web;
-
-            return cityImage;
-        }
-
-        const getCityDetailsFromId = async (geoname_id, id_data) => {
-            if (geoname_id === "1650527") {
-                let urbanScores = "https://api.teleport.org/api/urban_areas/slug:bali/";
-                
-                return urbanScores;
-            }
-
-            else {
-                let urbanScores = id_data.data["_links"]["city:urban_area"]["href"];
-                
-                return urbanScores;
-            }
-        };
-
-        const getCoordFromIdData = async id_data => {
-            coord = id_data.data["location"]["latlon"];
-            return coord;
-        }
-
-        geoname_results = await getCityIdFromSearchTerm(searchTerm);
-
-        if(geoname_results.geoname === 0) {
-            results = {
-            urbanScores: 'N/A',
-            geoname_id: 'N/A',
-            cityImage: 'N/A',
-            cityName: 'N/A',
-            coord: 'N/A',
-            match: true,
-            chosenCity: 'N/A',
-            inDatabase: false
-            }
-            return results;
-        }
-        else if (geoname_results.match) {
-            id_data = await fromCityId(geoname_results.geoname);
-            urbanScores = await getCityDetailsFromId(geoname_results.geoname, id_data);
-            cityImage = await getCityImageFromUrbanScores(urbanScores);
-            cityName = await getCityName(id_data);
-            coord = await getCoordFromIdData(id_data);
-
-            results = {
-              urbanScores,
-              geoname_id: geoname_results.geoname,
-              cityImage,
-              cityName,
-              coord,
-              match: true,
-              closestCity: geoname_results.urban_area,
-              inDatabase: true
-            };
-
-            return results; 
-        }
-        else {
-            closestCity = await getCityName(await fromCityId(geoname_results.geoname));
-            geoname_id = await getCityIdFromUrbanName(
-              geoname_results.urban_area
-            );
-            id_data = await fromCityId(geoname_id);
-            urbanScores = await getCityDetailsFromId(
-                geoname_id,
-                id_data
-            );
-            cityImage = await getCityImageFromUrbanScores(
-                urbanScores
-            );
-            cityName = await getCityName(id_data);
-            coord = await getCoordFromIdData(id_data);
-        }
-
-        results = {
-            urbanScores,
-            geoname_id: geoname_id,
-            cityImage,
-            cityName,
-            coord,
-            match: false,
-            closestCity,
-            inDatabase: true
-        };
-
-        return results; 
+  return results;
 };
-
