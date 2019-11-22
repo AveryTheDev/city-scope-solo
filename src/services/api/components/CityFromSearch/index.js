c7682077d120cbb92321bfdc7ba3da6974e0e520
abrown945 <abrown945@gmail.com>
2019-11-22 14:35:12 -0500

    redirect

--- src/services/api/components/CityFromSearch/index.js
+++ src/services/api/components/CityFromSearch/index.js
@@ -11,21 +11,40 @@ export const getCityBySearchTerm = async city => {
     coord,
     closestCity,
     results;
+
   let geoname_results = {
     geoname: 0,
     urban_area: "",
     match: false
   };
 
+  /* 
+    Is used to make sure that all of the information returned from a search query
+    matches the city provided.
+
+    Without this, there will be a discrepancy in population data returned
+  */
+
   const fromCityId = async geoname_id => {
     id_data = await teleport.get("cities/geonameid:" + geoname_id);
     return id_data;
   };
 
+  /*
+    uses the geoname id corresponding with a successful search term result to
+   to present the properly formatted version of the city name
+  */
+
   const getCityName = async id_data => {
     cityName = id_data.data["name"];
     return cityName;
   };
+
+  /* 
+    written to protect against instances when a search term initally returns a
+    successful request but an inconsistency in api city name results in 
+    the failed continuation of reuqests for further information
+  */
 
   const cityMatchCheck = async geoname_id => {
     const id = geoname_id;
@@ -79,6 +98,11 @@ export const getCityBySearchTerm = async city => {
     }
   };
 
+  /* 
+    Only processes city inquires placed directly in the search bar.
+    If query fails, the requests stops and a failed search page rerouting is triggered
+  */
+
   const getCityIdFromSearchTerm = async searchTerm => {
     if (typeof searchTerm !== "string") {
       searchTerm = Object.values(searchTerm).toString();
@@ -123,6 +147,12 @@ export const getCityBySearchTerm = async city => {
     }
   };
 
+  /* 
+    used in the instance that one of the city names provided by teleport is
+    selected via one of the list items available on the landing page or 
+    failed search page
+  */
+
   const getCityIdFromUrbanName = async urban_name => {
     if (
       urban_name.includes("minneapolis-saint") ||
@@ -150,6 +180,11 @@ export const getCityBySearchTerm = async city => {
     return geoname_id;
   };
 
+  /* 
+    if the urban scores are from request are valid, that endpoint is used 
+    to retrieve img urls for use
+  */
+
   const getCityImageFromUrbanScores = async urbanScores => {
     let imageURL = await teleport.get(urbanScores);
     imageURL = imageURL.data["_links"]["ua:images"]["href"];
@@ -159,6 +194,12 @@ export const getCityBySearchTerm = async city => {
 
     return cityImage;
   };
+
+  /* 
+    Utilitzes geoname id or id_data (which is same format but sourced from diff functions),
+    to retrieve the urban score link which provides in depth statistical information about the city
+    such as housing costs, avg salary and PISA scores (education) as well as img of the city
+  */
 
   const getCityDetailsFromId = async (geoname_id, id_data) => {
     if (geoname_id === "1650527") {
@@ -170,12 +211,20 @@ export const getCityBySearchTerm = async city => {
     }
   };
 
+  /* to be used for future features that have been yet to be implemented */
+
   const getCoordFromIdData = async id_data => {
     coord = id_data.data["location"]["latlon"];
     return coord;
   };
 
+  /* First function that runs which determines whether search term entered will
+    return valid results in the api
+  */
+
   geoname_results = await getCityIdFromSearchTerm(searchTerm);
+
+  /* no match in database */
 
   if (geoname_results.geoname === 0) {
     results = {
@@ -190,6 +239,10 @@ export const getCityBySearchTerm = async city => {
     };
     return results;
   } else if (geoname_results.match) {
+    /* 
+    runs in the instance that the search term is identical in format to the 
+    city name in the database
+  */
     id_data = await fromCityId(geoname_results.geoname);
     urbanScores = await getCityDetailsFromId(geoname_results.geoname, id_data);
     cityImage = await getCityImageFromUrbanScores(urbanScores);
@@ -208,6 +261,14 @@ export const getCityBySearchTerm = async city => {
     };
     return results;
   } else {
+    /* 
+    Runs when the search term is similar enough to the correct pathing to 
+    provide a close city match according to the api but requires additional functions 
+    to make it clear to the user that Teleport's API is providing the closest city
+    in its database as a result.
+
+    Also prevents the wrong population data from being returned.
+  */
     closestCity = await getCityName(await fromCityId(geoname_results.geoname));
     geoname_id = await getCityIdFromUrbanName(geoname_results.urban_area);
     id_data = await fromCityId(geoname_id);
@@ -223,6 +284,11 @@ export const getCityBySearchTerm = async city => {
     cityImage,
     cityName,
     coord,
+    /* 
+      false value will trigger a modal upon loading of city to inform user that the
+      api is using information on a city close in proximity rather than their 
+      specific city
+     */
     match: false,
     closestCity,
     inDatabase: true
